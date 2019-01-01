2f293c1c06d1734e986f43e8495524b697fadf17
Karim Ljung <karim.ljung@gmail.com>
2019-01-01 20:05:26 +0100

    Adding domain model

---
+++ DomainModel/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace DomainModel
+{
+    public class Product
+    {
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+    }
+}
---
+++ DomainModel/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DomainModel
+{
+    public class Transaction
+    {
+        public string Id { get; set; }
+        public string ProductId { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
