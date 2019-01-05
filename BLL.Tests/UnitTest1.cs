6321fc00eb82e91d3c9060823c6ab2dae4bf31dc
Karim Ljung <karim.ljung@gmail.com>
2019-01-05 19:53:10 +0100

    Adding AutoFixture and AutoMocking

--- BLL.Tests/UnitTest1.cs
+++ BLL.Tests/UnitTest1.cs
@@ -1,18 +1,25 @@
+using AutoFixture;
+using AutoFixture.AutoMoq;
 using NUnit.Framework;
+
 
 namespace Tests
 {
+    [TestFixture]
     public class Tests
     {
+        private IFixture _fixture;
         [SetUp]
         public void Setup()
         {
+            _fixture = new Fixture().Customize(new AutoMoqCustomization { ConfigureMembers = true });
         }
 
         [Test]
         public void Test1()
         {
             Assert.Pass();
+            var newString = _fixture.Create<string>();
         }
     }
 }
