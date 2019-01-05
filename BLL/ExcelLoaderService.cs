884cf80f60940a3327d102317efc5969b2e27ac8
Karim Ljung <karim.ljung@gmail.com>
2019-01-05 21:22:21 +0100

    Adding excel dummy functionality

--- BLL/ExcelLoaderService.cs
+++ BLL/ExcelLoaderService.cs
@@ -14,6 +14,16 @@ namespace BLL
             {
                 var workbook = package.Workbook;
                 var worksheet = workbook.Worksheets.First();
+                var colCount = worksheet.Dimension.End.Column;  //get Column Count
+                var rowCount = worksheet.Dimension.End.Row;     //get row count
+                for (int row = 1; row <= rowCount; row++)
+                {
+                    for (int col = 1; col <= colCount; col++)
+                    {
+                        var value = worksheet.Cells[row, col].Value.ToString().Trim();
+                        Console.WriteLine(" Row:" + row + " column:" + col + " Value:" + worksheet.Cells[row, col].Value.ToString().Trim());
+                    }
+                }
             }
 
                 throw new NotImplementedException();
